cc264e317a2fa31228220ca7eb02a4f1548c1fcb
gobroni tsitlauri <deathshadow075@gmail.com>
2024-07-14 05:15:19 -0700

    second exam for me

---
+++ leaders test #3/#4 (7 - kyu)/How many are smaller than me.py
@@ -0,0 +1,10 @@
+def smaller(arr):   
+    arr2 = [0] * len(arr)
+    the_len = len(arr)
+    for i in range(the_len):
+        count = 0
+        for x in range(i+1,the_len):
+            if arr[x] < arr[i]:
+                count += 1
+        arr2[i] = count
+    return arr2
